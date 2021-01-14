627627a99097f652d15a5082d06110de48ab31f6
daliayan <daliayan5@gmail.com>
2021-01-13 21:00:48 -0500

    "Methods"

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def greetings_friend(name)
+    puts "Hello #{name}."
+end
+
+def farewell_sayings(saying, title)
+    puts "#{saying} to you my #{title}!"
+end
+
+farewell_sayings("Adios", "Amigo")
